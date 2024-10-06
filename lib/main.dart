2d5033199943d526f3fbc22e1be25239daf8f64a
tiendev2003 <tientc.21it@vku.udn.vn>
2024-10-06 19:01:59 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: const Text('Ask Me Anything'),
+      ),
+      body: const Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('assets/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
