a298fe098c3f35410f3dffe2abd158d1ba25d68c
tiendev2003 <tientc.21it@vku.udn.vn>
2024-10-06 19:03:15 +0700

    td

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
